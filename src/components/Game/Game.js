3e2c3741157bbc20b62e48199a9f39b4c99a9e46
Paige Clark <paige.clark@live.com>
2026-03-12 21:30:22 -0400

    Add exercise 1 solution

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,15 +1,56 @@
-import React from 'react';
+import React from "react";
 
-import { sample } from '../../utils';
-import { WORDS } from '../../data';
+import { sample } from "../../utils";
+import { WORDS } from "../../data";
+import { GUESS_LENGTH, MAX_INPUT_CHARS } from "../../constants";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
+
 // To make debugging easier, we'll log the solution in the console.
 console.info({ answer });
 
 function Game() {
-  return <>Put a game here!</>;
+  return (
+    <>
+      <GuessInput />
+    </>
+  );
+}
+
+function GuessInput() {
+  const [guess, setGuess] = React.useState("");
+
+  const guessPatternRegex = `[a-zA-Z]{${GUESS_LENGTH}}`;
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const submission = { guess };
+    console.log(submission);
+    setGuess("");
+  }
+
+  return (
+    <form
+      className="guess-input-wrapper"
+      onSubmit={handleSubmit}
+      autoComplete="off"
+    >
+      <label htmlFor="guess-input">Enter a {GUESS_LENGTH} letter guess: </label>
+      <input
+        required
+        id="guess-input"
+        type="text"
+        maxLength={GUESS_LENGTH}
+        pattern={guessPatternRegex}
+        value={guess}
+        onChange={(event) => {
+          const updatedGuess = event.target.value.toUpperCase();
+          setGuess(updatedGuess);
+        }}
+      />
+    </form>
+  );
 }
 
 export default Game;
